55c9246c3ff24385ba36207e9e2a9ac738573f6a
apilifogladif <filipajfidalgo@gmail.com>
2023-11-22 16:38:11 +0000

    Finished main menu

--- src/main/java/com/projLDTS/blackjack/Menus/MainMenu.java
+++ src/main/java/com/projLDTS/blackjack/Menus/MainMenu.java
@@ -4,19 +4,37 @@ import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.input.MouseAction;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 import com.googlecode.lanterna.TerminalPosition;
 
 import java.io.IOException;
+import java.util.Vector;
 
 public class MainMenu {
     private final TerminalScreen screen;
     private final int width = 100;
     private final int height = 35;
+    private final TerminalPosition startButtonPos = new TerminalPosition(35, 18);
+    private final TerminalPosition howToPlayButtonPos = new TerminalPosition(35, 22);
+    private final TerminalPosition exitButtonPos = new TerminalPosition(35, 26);
+    private final TerminalPosition startStringPos = new TerminalPosition(48, 19);
+    private final TerminalPosition howToPlayStringPos = new TerminalPosition(45, 23);
+    private final TerminalPosition exitStringPos = new TerminalPosition(48, 27);
+    private final TerminalSize buttonSize = new TerminalSize(30, 3);
+    private final TextColor buttonColor = TextColor.Factory.fromString("#00CA4C");
+    private final TextColor selectedButtonColor = TextColor.Factory.fromString("#FF0000");
+    private final Vector<String> buttons = new Vector<>();
+    int buttonSelected;
 
     public MainMenu() throws IOException {
+        buttonSelected = 0;
+        buttons.add("START");
+        buttons.add("HOW TO PLAY");
+        buttons.add("EXIT");
+
         Terminal terminal = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(width, height)).createTerminal();
         screen = new TerminalScreen(terminal);
 
@@ -44,29 +62,76 @@ public class MainMenu {
         textGraphics.putString(new TerminalPosition(9, 13), "                                              ,BB                                  \n");
         textGraphics.putString(new TerminalPosition(9, 14), "                                            BBBP\"\n");
 
-        //start button
-        textGraphics.setForegroundColor(TextColor.Factory.fromString("#000000"));
-        textGraphics.setBackgroundColor(TextColor.Factory.fromString("#00CA4C"));
-        textGraphics.fillRectangle(new TerminalPosition(35, 18),new TerminalSize(30, 3),' ');
-        textGraphics.putString(new TerminalPosition(48, 19), "START");
-
-        //how to play button
-        textGraphics.setForegroundColor(TextColor.Factory.fromString("#000000"));
-        textGraphics.setBackgroundColor(TextColor.Factory.fromString("#00CA4C"));
-        textGraphics.fillRectangle(new TerminalPosition(35, 22),new TerminalSize(30, 3),' ');
-        textGraphics.putString(new TerminalPosition(45, 23), "HOW TO PLAY");
-
-        //exit button
-        textGraphics.setForegroundColor(TextColor.Factory.fromString("#000000"));
-        textGraphics.setBackgroundColor(TextColor.Factory.fromString("#00CA4C"));
-        textGraphics.fillRectangle(new TerminalPosition(35, 26),new TerminalSize(30, 3),' ');
-        textGraphics.putString(new TerminalPosition(48, 27), "EXIT");
-
+        // buttons
+        drawButton(selectedButtonColor, textGraphics, startButtonPos, startStringPos, "START");
+        drawButton(buttonColor, textGraphics, howToPlayButtonPos, howToPlayStringPos, "HOW TO PLAY");
+        drawButton(buttonColor, textGraphics, exitButtonPos, exitStringPos, "EXIT");
         screen.refresh();
+
+
+        while (true) {
+            KeyStroke key = screen.readInput();
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q')
+                screen.close();
+            if (key.getKeyType() == KeyType.EOF)
+                break;
+            boolean arrow = processKey(key);
+            if (!arrow) continue;
+            changeSelectedButton(textGraphics);
+            screen.refresh();
+        }
     }
 
-    private void processKey(KeyStroke key) {
-        //board.processKey(key);
+    private void changeSelectedButton(TextGraphics textGraphics) {
+        if (buttonSelected == 0) {
+            drawButton(buttonColor, textGraphics, exitButtonPos, exitStringPos, "EXIT");
+            drawButton(selectedButtonColor, textGraphics, startButtonPos, startStringPos, "START");
+            drawButton(buttonColor, textGraphics, howToPlayButtonPos, howToPlayStringPos, "HOW TO PLAY");
+        }
+        if (buttonSelected == 1) {
+            drawButton(buttonColor, textGraphics, startButtonPos, startStringPos, "START");
+            drawButton(selectedButtonColor, textGraphics, howToPlayButtonPos, howToPlayStringPos, "HOW TO PLAY");
+            drawButton(buttonColor, textGraphics, exitButtonPos, exitStringPos, "EXIT");
+        }
+        if (buttonSelected == 2) {
+            drawButton(buttonColor, textGraphics, startButtonPos, startStringPos, "START");
+            drawButton(buttonColor, textGraphics, howToPlayButtonPos, howToPlayStringPos, "HOW TO PLAY");
+            drawButton(selectedButtonColor, textGraphics, exitButtonPos, exitStringPos, "EXIT");
+        }
+    }
+
+    private boolean processKey(KeyStroke key) {
+        if (key.getKeyType() == KeyType.ArrowUp) {
+            buttonSelected--;
+            if (buttonSelected == -1) buttonSelected = 2;
+            return true;
+        }
+        else if (key.getKeyType() == KeyType.ArrowDown) {
+            buttonSelected++;
+            if (buttonSelected == 3) buttonSelected = 0;
+            return true;
+        }
+        else if (key.getKeyType() == KeyType.Enter) {
+            switch (buttonSelected) {
+                case 0:
+                    //call start menu
+                    break;
+                case 1:
+                    //call how to play
+                    break;
+                default:
+                    //call exit
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private void drawButton(TextColor color, TextGraphics textGraphics, TerminalPosition buttonPos,TerminalPosition stringPos, String str) {
+        textGraphics.setForegroundColor(TextColor.Factory.fromString("#000000"));
+        textGraphics.setBackgroundColor(color);
+        textGraphics.fillRectangle(buttonPos,buttonSize,' ');
+        textGraphics.putString(stringPos, str);
     }
 
     public void run() throws IOException {
@@ -77,7 +142,6 @@ public class MainMenu {
                 screen.close();
             if (key.getKeyType() == KeyType.EOF)
                 break;
-            processKey(key);
         }
     }
 }
