0a381ef4262c6fce4c9ca441b1baa4e999fe47c9
g3raldes <g3raldes@gmail.com>
2023-11-24 09:19:40 +0000

    Corrected some errors on HowToPlay

--- src/main/java/com/projLDTS/blackjack/Menus/HowToPlay.java
+++ src/main/java/com/projLDTS/blackjack/Menus/HowToPlay.java
@@ -15,14 +15,13 @@ import java.io.IOException;
 
 public class HowToPlay {
     private final TerminalScreen screen;
-    private final int width = 100;
-    private final int height = 35;
+    private final int width = 130;
+    private final int height = 40;
     Button ret1;
     Button ret2;
     Button next;
     Button previous;
     TextColor buttonColor = TextColor.Factory.fromString("#00CA4C");
-    TextColor selectedColor = TextColor.Factory.fromString("#FF0000");
     int buttonSelected;
     int currentPage;
     public HowToPlay(TerminalScreen screen_) throws IOException {
@@ -33,10 +32,10 @@ public class HowToPlay {
         screen.setCursorPosition(null);
         screen.startScreen();
 
-        ret1 = new Button(new TerminalPosition(18, 30), " RETURN", new TerminalSize(15, 3), new TerminalPosition(15, 29), selectedColor);
-        next = new Button(new TerminalPosition(76, 30), "-->", new TerminalSize(15, 3), new TerminalPosition(70, 29), buttonColor);
-        previous = new Button(new TerminalPosition(18, 30), "<--", new TerminalSize(15, 3), new TerminalPosition(15, 29), buttonColor);
-        ret2 = new Button(new TerminalPosition(76, 30), " RETURN", new TerminalSize(15, 3), new TerminalPosition(70, 29), buttonColor);
+        ret1 = new Button(new TerminalPosition(30, 34), "RETURN", new TerminalSize(50, 3), new TerminalPosition(9, 33), buttonColor);
+        next = new Button(new TerminalPosition(94, 34), "-->", new TerminalSize(50, 3), new TerminalPosition(70, 33), buttonColor);
+        previous = new Button(new TerminalPosition(30, 34), "<--", new TerminalSize(50, 3), new TerminalPosition(9, 33), buttonColor);
+        ret2 = new Button(new TerminalPosition(94, 34), "RETURN", new TerminalSize(50, 3), new TerminalPosition(70, 33), buttonColor);
     }
     public void run() throws IOException {
         while (true) {
@@ -68,7 +67,6 @@ public class HowToPlay {
                 break;
             boolean arrow = processKey(key);
             if (!arrow) continue;
-            changeSelectedButton(textGraphics);
             screen.refresh();
         }
     }
@@ -87,20 +85,14 @@ public class HowToPlay {
     private void drawPageButtons(TextGraphics textGraphics) {
         switch (currentPage) {
             case 1:
-                ret1.setColor(selectedColor);
-                next.setColor(selectedColor);
                 ret1.drawButton(textGraphics);
                 next.drawButton(textGraphics);
                 break;
             case 2, 3:
-                previous.setColor(selectedColor);
-                next.setColor(selectedColor);
                 previous.drawButton(textGraphics);
                 next.drawButton(textGraphics);
                 break;
             case 4:
-                previous.setColor(selectedColor);
-                ret2.setColor(selectedColor);
                 previous.drawButton(textGraphics);
                 ret2.drawButton(textGraphics);
                 break;
@@ -119,7 +111,7 @@ public class HowToPlay {
                 textGraphics.putString(new TerminalPosition(9, 22), "   - Number cards (2-10) are worth their face value.\n");
                 textGraphics.putString(new TerminalPosition(9, 23), "   - Face cards (Jack, Queen, King) are each worth 10.");
                 textGraphics.putString(new TerminalPosition(9, 24), "   - Aces can be worth 1 or 11, depending on which value benefits the hand more.\n");
-                textGraphics.putString(new TerminalPosition(50, 27), currentPage + "/4");
+                textGraphics.putString(new TerminalPosition(63, 30), currentPage + "/4");
                 break;
             case 2:
                 drawTitle(textGraphics);
@@ -132,7 +124,7 @@ public class HowToPlay {
                 textGraphics.putString(new TerminalPosition(9, 25), "   - If a player's total is closer to 21 than the dealer's, they win.\n");
                 textGraphics.putString(new TerminalPosition(9, 26), "   - If the dealer busts and the player doesn't, the player wins.\n");
                 textGraphics.putString(new TerminalPosition(9, 27), "   - If both the player and dealer have the same total, it's a push (no one wins or loses).\n");
-                textGraphics.putString(new TerminalPosition(50, 30), currentPage + "/4");
+                textGraphics.putString(new TerminalPosition(63, 30), currentPage + "/4");
                 break;
             case 3:
                 drawTitle(textGraphics);
@@ -142,105 +134,50 @@ public class HowToPlay {
                 textGraphics.putString(new TerminalPosition(9, 20), "   - **Split:** If dealt two cards of the same rank, split them into two separate hands, each with its bet.\n");
                 textGraphics.putString(new TerminalPosition(9, 23), "6. **Blackjack:**\n");
                 textGraphics.putString(new TerminalPosition(9, 24), "   - A \"Blackjack\" is an Ace and a 10-value card. It usually pays 3:2.\n");
-                textGraphics.putString(new TerminalPosition(50, 30), currentPage + "/4");
+                textGraphics.putString(new TerminalPosition(63, 30), currentPage + "/4");
                 break;
             case 4:
                 drawTitle(textGraphics);
                 // Draw content for page 2
-                textGraphics.putString(new TerminalPosition(9, 16), "7. **Tips:**\n");
-                textGraphics.putString(new TerminalPosition(9, 17), "   - Pay attention to the dealer's upcard and adjust your strategy accordingly.\n");
-                textGraphics.putString(new TerminalPosition(9, 18), "   - Practice basic strategy to maximize your chances of winning.\n");
+                textGraphics.putString(new TerminalPosition(9, 17), "7. **Tips:**\n");
+                textGraphics.putString(new TerminalPosition(9, 18), "   - Pay attention to the dealer's upcard and adjust your strategy accordingly.\n");
+                textGraphics.putString(new TerminalPosition(9, 19), "   - Practice basic strategy to maximize your chances of winning.\n");
                 textGraphics.putString(new TerminalPosition(9, 21), "8. **Commands:**\n");
                 textGraphics.putString(new TerminalPosition(9, 22), "   - Type 'h' to Hit.\n");
                 textGraphics.putString(new TerminalPosition(9, 23), "   - Type 's' to Stand.\n");
                 textGraphics.putString(new TerminalPosition(9, 24), "   - Type 'd' to Double Down.\n");
                 textGraphics.putString(new TerminalPosition(9, 25), "   - Type 'p' to Split (if eligible).\n");
-                textGraphics.putString(new TerminalPosition(9, 28), "9. **Have Fun and Good Luck!**\n");
-                textGraphics.putString(new TerminalPosition(50, 30), currentPage + "/4");
+                textGraphics.putString(new TerminalPosition(9, 27), "9. **Have Fun and Good Luck!**\n");
+                textGraphics.putString(new TerminalPosition(63, 30), currentPage + "/4");
                 break;
             default:
                 break;
         }
     }
-    private void changeSelectedButton(TextGraphics textGraphics) {
-        switch (currentPage) {
-            case 1:
-                if (buttonSelected == 0) {
-                    ret1.setColor(selectedColor);
-                    next.setColor(buttonColor);
-                    ret1.drawButton(textGraphics);
-                    next.drawButton(textGraphics);
-                }
-                if (buttonSelected == 1) {
-                    ret1.setColor(buttonColor);
-                    next.setColor(selectedColor);
-                    ret1.drawButton(textGraphics);
-                    next.drawButton(textGraphics);
-                }
-            case 2, 3:
-                if (buttonSelected == 0) {
-                    previous.setColor(selectedColor);
-                    next.setColor(buttonColor);
-                    previous.drawButton(textGraphics);
-                    next.drawButton(textGraphics);
-                }
-                if (buttonSelected == 1) {
-                    previous.setColor(buttonColor);
-                    next.setColor(selectedColor);
-                    previous.drawButton(textGraphics);
-                    next.drawButton(textGraphics);
-                }
-            case 4:
-                if (buttonSelected == 0) {
-                    previous.setColor(selectedColor);
-                    ret2.setColor(buttonColor);
-                    previous.drawButton(textGraphics);
-                    ret2.drawButton(textGraphics);
-                }
-                if (buttonSelected == 1) {
-                    previous.setColor(buttonColor);
-                    ret2.setColor(selectedColor);
-                    previous.drawButton(textGraphics);
-                    ret2.drawButton(textGraphics);
-                }
-        }
 
-    }
     private boolean processKey(KeyStroke key) throws IOException {
         if (key.getKeyType() == KeyType.ArrowLeft) {
             if (currentPage > 1) {
                 currentPage--;
                 buttonSelected = 0;
+                handlePreviousPage();
                 drawHowToPlay();
-            } else if (currentPage == 2) {
-                handleNextPage();
+            } else {
+                screen.close();
+                new MainMenu().run();
             }
             return true;
         } else if (key.getKeyType() == KeyType.ArrowRight) {
             if (currentPage < 4) {
                 currentPage++;
                 buttonSelected = 0;
+                handleNextPage();
                 drawHowToPlay();
-            } else if (currentPage == 2) {
-                handlePreviousPage();
+            } else {
+                screen.close();
+                new MainMenu().run();
             }
             return true;
-        } else if (key.getKeyType() == KeyType.Enter) {
-            switch (buttonSelected) {
-                case 0:
-                    screen.close();
-                    new MainMenu().run();
-                    break;
-                case 1:
-                    if (currentPage == 2) {
-                        handleNextPage();
-                    }
-                    break;
-                default:
-                    screen.close();
-                    break;
-            }
-            return false;
         }
         return false;
     }
@@ -252,10 +189,7 @@ public class HowToPlay {
     }
     private void handlePreviousPage() throws IOException {
         switch (currentPage) {
-            case 2:
-                drawHowToPlay();
-                break;
-            case 3:
+            case 2, 3:
                 drawHowToPlay();
                 break;
             default:
