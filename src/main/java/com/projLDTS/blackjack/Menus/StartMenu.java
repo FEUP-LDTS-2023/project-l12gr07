dd4e3d8bb4ab28a221e0aae8c7312c13ab3e1fa3
Leonor2004 <up202205796@up.pt>
2023-11-24 17:05:23 +0000

    StartMenu Buttons problem

--- src/main/java/com/projLDTS/blackjack/Menus/StartMenu.java
+++ src/main/java/com/projLDTS/blackjack/Menus/StartMenu.java
@@ -1,24 +1,16 @@
 package com.projLDTS.blackjack.Menus;
-import Elements.Button;
+import com.projLDTS.blackjack.Elements.Button;
 import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
-import com.googlecode.lanterna.gui2.Borders;
-import com.googlecode.lanterna.gui2.Component;
-import com.googlecode.lanterna.gui2.TextBox;
-import com.googlecode.lanterna.gui2.TextGUIGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.input.KeyType;
-import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
-import com.projLDTS.blackjack.Board;
-import com.googlecode.lanterna.input.KeyStroke;
 
 import java.io.IOException;
-import java.util.regex.Pattern;
 
 
 public class StartMenu {
