d7c86b6dd3858134404d7d4c49e4e39325844299
g3raldes <g3raldes@gmail.com>
2023-12-13 10:30:53 +0000

    Added BaseCard class to draw the card

---
+++ src/main/java/com/projLDTS/blackjack/model/game/Cards/BaseCard.java
@@ -0,0 +1,51 @@
+package com.projLDTS.blackjack.model.game.Cards;
+
+import java.util.ArrayList;
+
+public class BaseCard {
+    ArrayList<String> playingCard = new ArrayList<>();
+    public BaseCard(int rank, String suit){
+        String rankString;
+        switch (suit) {
+            case "Hearts":
+                suit = "♥";
+                break;
+            case "Diamonds":
+                suit = "♦";
+                break;
+            case "Clubs":
+                suit = "♣";
+                break;
+            case "Spades":
+                suit = "♠";
+                break;
+        }
+        switch (rank) {
+            case 11:
+                rankString = "J";
+                break;
+            case 12:
+                rankString = "Q";
+                break;
+            case 13:
+                rankString = "K";
+                break;
+            case 1:
+                rankString = "A";
+                break;
+            default:
+                rankString = Integer.toString(rank);
+                break;
+        }
+        playingCard.add("┌─────────┐");
+        playingCard.add("│" + rankString + "        │");
+        playingCard.add("│         │");
+        playingCard.add("│         │");
+        playingCard.add("│    " + suit + "    │");
+        playingCard.add("│         │");
+        playingCard.add("│         │");
+        playingCard.add("│       " + rankString + " │");
+        playingCard.add("└─────────┘");
+    }
+
+}
