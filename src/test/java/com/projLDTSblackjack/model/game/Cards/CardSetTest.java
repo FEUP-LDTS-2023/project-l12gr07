bcf2938982b9cf67d3f60ff84ad4122481c1c119
g3raldes <g3raldes@gmail.com>
2023-12-20 16:06:07 +0000

    Added CardSet and Card Tests

--- src/test/java/com/projLDTSblackjack/model/game/Cards/CardSetTest.java
+++ src/test/java/com/projLDTSblackjack/model/game/Cards/CardSetTest.java
@@ -1,4 +1,41 @@
 package com.projLDTSblackjack.model.game.Cards;
 
+import com.projLDTS.blackjack.model.game.Cards.Card;
+import com.projLDTS.blackjack.model.game.Cards.CardSet;
+import com.projLDTS.blackjack.model.game.Cards.Hand;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class CardSetTest {
+    private CardSet cardSet;
+    private org.mockito.Mockito Mockito;
+
+    @BeforeEach
+    void setUp() {
+        cardSet = new CardSet();
+    }
+
+    @Test
+    void testCardSetInitialization() {
+        Assertions.assertNotNull(cardSet);
+        Assertions.assertNotNull(cardSet.getHand());
+    }
+
+    @Test
+    void testGetHand() {
+        Hand hand = cardSet.getHand();
+        Assertions.assertNotNull(hand);
+    }
+
+    @Test
+    void testGetHandSameInstance() {
+        Hand hand1 = cardSet.getHand();
+        Hand hand2 = cardSet.getHand();
+        Assertions.assertSame(hand1, hand2);
+    }
+
 }
