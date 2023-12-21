583822487cd763789456a9b97716f846cfa8d6cc
g3raldes <g3raldes@gmail.com>
2023-12-21 11:49:45 +0000

    Began testing GameSet and Deck classes

--- src/test/java/com/projLDTSblackjack/model/game/Decks/GameSetTest.java
+++ src/test/java/com/projLDTSblackjack/model/game/Decks/GameSetTest.java
@@ -1,4 +1,46 @@
 package com.projLDTSblackjack.model.game.Decks;
 
+import com.projLDTS.blackjack.model.game.Cards.Card;
+import com.projLDTS.blackjack.model.game.Cards.Hand;
+import com.projLDTS.blackjack.model.game.Cards.Player;
+import com.projLDTS.blackjack.model.game.Cards.Dealer;
+import com.projLDTS.blackjack.gui.UserInput;
+
+import com.projLDTS.blackjack.model.game.Decks.Deck;
+import com.projLDTS.blackjack.model.game.Decks.GameSet;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 public class GameSetTest {
+    private Deck mockDeck;
+    private UserInput mockUserInput;
+
+    private GameSet gameSet;
+
+    @BeforeEach
+    void setUp() {
+        gameSet = new GameSet(1);
+    }
+
+    @Test
+    void testInitialization() {
+        assertNotNull(gameSet.getPlayer());
+        assertNotNull(gameSet.getDealer());
+        assertNotNull(gameSet.getDeck());
+    }
+    @Test
+    void testDealingCards() {
+        int initialPlayerHandSize = gameSet.getPlayer().getHand().getHand().size();
+        int initialDealerHandSize = gameSet.getDealer().getHand().getHand().size();
+        gameSet.dealCards();
+        assertEquals(initialPlayerHandSize + 2, gameSet.getPlayer().getHand().getHand().size());
+        assertEquals(initialDealerHandSize + 1, gameSet.getDealer().getHand().getHand().size());
+    }
+
 }
